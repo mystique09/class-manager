e34bb9e80bda824da3c43f5fbb4464a4546248e0
mystique09 <knightkrypto7@gmail.com>
2022-05-10 00:38:57 +0800

    add: navbar component

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	let toggle = false;
+
+	const toggleNav = () => {
+		toggle = !toggle;
+	};
+</script>
+
+<nav class="navbar bg-gray-700 flex flex-col items-end">
+	<div class="flex flex-row w-full">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghost text-xl normal-case">Class Manager</a>
+		</div>
+		<div class="flex-none">
+			<button on:click={() => (toggle = !toggle)} class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-5 h-5 stroke-current"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/></svg
+				>
+			</button>
+		</div>
+	</div>
+	<div class="menu w-full flex flex-col items-end" class:hidden={!toggle}>
+		<div class="divider" />
+		<menu>
+			<li>
+				<a on:click={toggleNav} class="btn btn-ghost btn-md" href="/auth">Login</a>
+			</li>
+			<li>
+				<a on:click={toggleNav} class="btn btn-ghost btn-md" href="/signup">Signup</a>
+			</li>
+			<li>
+				<a on:click={toggleNav} class="btn btn-ghost btn-md" href="/#about">About Us</a>
+			</li>
+		</menu>
+	</div>
+</nav>
