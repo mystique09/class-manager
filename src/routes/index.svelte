1cc8a7a75b8cd9076cf55bf7b490b949c8c5a64f
mystique09 <knightkrypto7@gmail.com>
2022-05-10 22:33:41 +0800

    feat: lorem text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,13 @@
 <div class="hero min-h-screen bg-gray-700">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">Class Manager</h1>
-			<p class="py-6">Manage your class records without hussle!</p>
-			<a href="/signup" class="btn btn-accent btn-md">Get Started</a>
+			<h1 class="text-4xl font-bold">Class Management System</h1>
+			<p class="py-6 mb-2 font-light">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quasi distinctio,
+				accusantium dolor esse est quas qui nisi perferendis quisquam amet iure eius repellat,
+				recusandae incidunt unde et dicta voluptatem?
+			</p>
+			<a href="/signup" class="btn btn-accent btn-wide">Get Started</a>
 		</div>
 	</div>
 </div>
-
