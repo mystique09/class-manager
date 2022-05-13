4477f1664c3d493da72f00d3ffd9c557946b1dd9
mystique09 <knightkrypto7@gmail.com>
2022-05-13 12:34:37 +0800

    add: hooks

---
+++ src/hooks.ts
@@ -0,0 +1,21 @@
+import type { RequestEvent, MaybePromise, RequestOptions } from "@sveltejs/kit/types/private";
+import { parse } from 'cookie';
+
+const empty_user_session: UserSession = {
+  id: null,
+  username: null,
+};
+
+export async function handle({ event, resolve }: { event: RequestEvent, resolve: (event: RequestEvent, opts?: RequestOptions) => MaybePromise<Response> }) {
+  const cookie = parse(event.request.headers.get("cookie") || '');
+  const user_session: UserSession = cookie.session ? JSON.parse(cookie.session) : empty_user_session;
+
+  event.locals.user = user_session;
+
+  const response = await resolve(event);
+  return response;
+}
+
+export async function getSession(event: RequestEvent) {
+  return event.locals.user;
+}
