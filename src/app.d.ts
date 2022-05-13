1511a5deae2513f35fb9768683e064495671c31a
mystique09 <knightkrypto7@gmail.com>
2022-05-13 12:34:07 +0800

    add: types

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,8 +3,23 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface Session {}
-	// interface Stuff {}
+  interface Locals {
+    user: UserSession | null
+  }
+  // interface Platform {}
+  // interface Session {}
+  // interface Stuff {}
+}
+
+interface Classroom {
+  id: string;
+  name: string;
+  section: string;
+  room: subject;
+  subject: string;
+}
+
+interface UserSession {
+  id: string | null;
+  username: string | null;
 }
