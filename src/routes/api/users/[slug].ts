dfdb7b3be980e1978239c42f3dcdc6262a7030e0
mystique09 <knightkrypto7@gmail.com>
2022-06-20 12:53:30 +0800

    add: initial endpoints

---
+++ src/routes/api/users/[slug].ts
@@ -0,0 +1,10 @@
+import type { RequestEvent } from "@sveltejs/kit";
+
+// endpoint to get one user
+export async function get(event: RequestEvent) {}
+
+// endpoint to update user
+export async function put(event: RequestEvent) {}
+
+// endpoint to delete user
+export async function del(event: RequestEvent) {}
