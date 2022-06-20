469f3ce5d901d8e53978ec2cb644fdb25353070b
mystique09 <knightkrypto7@gmail.com>
2022-06-20 17:37:31 +0800

    feat: return all users

--- src/routes/api/users/index.ts
+++ src/routes/api/users/index.ts
@@ -1,10 +1,13 @@
+import { getUsers } from "$lib/server/controllers/user.controller";
 import type { RequestEvent } from "@sveltejs/kit";
 
 // endpoint to get all user
-export function get() {
+export async function get() {
+  let users = await getUsers();
+
   return {
     status: 200,
-    body: 'TODO!'
+    body: users
   };
 }
 
