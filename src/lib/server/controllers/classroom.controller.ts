e7ef51ac77f2d59a2da6cbc6f77fad238dee7d90
mystique09 <knightkrypto7@gmail.com>
2022-06-20 15:24:46 +0800

    feat: import prisma from lib

--- src/lib/server/controllers/classroom.controller.ts
+++ src/lib/server/controllers/classroom.controller.ts
@@ -1,69 +1,67 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "$lib/prisma";
 
 // a type for user request, used to get user data in parameter
 type ClassRequest = {
-    name: string;
-    description: string;
-    section: string;
-    room: string;
-    subject: string;
+  name: string;
+  description: string;
+  section: string;
+  room: string;
+  subject: string;
 }
 
 // a controller to create new class
 export async function newClass(classReq: ClassRequest) {
-    const newClass = await prisma.classroom.create({
-        data: {
-            name: classReq.name,
-            description: classReq.description,
-            section: classReq.section,
-            room: classReq.room,
-            subject: classReq.subject
-        }
-    });
-    return newClass;
+  const newClass = await prisma.classroom.create({
+    data: {
+      name: classReq.name,
+      description: classReq.description,
+      section: classReq.section,
+      room: classReq.room,
+      subject: classReq.subject
+    }
+  });
+  return newClass;
 }
 
 // a controller to get all class
 export async function getClasses() {
-    const classes = await prisma.classroom.findMany().limit(10);
-    return classes;
+  const classes = await prisma.classroom.findMany().limit(10);
+  return classes;
 }
 
 // a controller to get class by id
 export async function getClass(id: string) {
-    const classroom = await prisma.classroom.findOne({
-        where: {
-            id
-        }
-    });
-    return classroom;
+  const classroom = await prisma.classroom.findOne({
+    where: {
+      id
+    }
+  });
+  return classroom;
 }
 
 // a controller to get update class by id
 export async function updateClass(id: string, classReq: ClassRequest) {
-    const updatedClass = await prisma.classroom.update({
-        where: {
-            id: id
-        },
-        data: {
-            name: classReq.name,
-            description: classReq.description,
-            section: classReq.section,
-            room: classReq.room,
-            subject: classReq.subject
-        }
-    });
-    return updatedClass;
+  const updatedClass = await prisma.classroom.update({
+    where: {
+      id: id
+    },
+    data: {
+      name: classReq.name,
+      description: classReq.description,
+      section: classReq.section,
+      room: classReq.room,
+      subject: classReq.subject
+    }
+  });
+  return updatedClass;
 }
 
 // a controller to get delete class by id
 export async function deleteClass(id: string) {
-    const deleteClass = await prisma.classroom.delete({
-        where: {
-            id: id
-        }
-    });
-    return deleteClass;
+  const deleteClass = await prisma.classroom.delete({
+    where: {
+      id: id
+    }
+  });
+  return deleteClass;
 }
