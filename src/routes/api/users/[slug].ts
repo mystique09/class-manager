201e8fa29e1376f4aa701781c1684bbeeb76cfc4
mystique09 <knightkrypto7@gmail.com>
2022-06-21 17:51:45 +0800

    add: get all users, and delete one user by id

--- src/routes/api/users/[slug].ts
+++ src/routes/api/users/[slug].ts
@@ -1,10 +1,13 @@
+import { deleteUser, getUser } from "$lib/server/controllers/user.controller";
 import type { RequestEvent } from "@sveltejs/kit";
 
 // endpoint to get one user
 export async function get(event: RequestEvent) {
+  let user = await getUser(event.params.slug);
+
   return {
     status: 200,
-    body: 'TODO!'
+    body: user
   };
 }
 
@@ -18,8 +21,10 @@ export async function put(event: RequestEvent) {
 
 // endpoint to delete user
 export async function del(event: RequestEvent) {
+  let user = await deleteUser(event.params.slug);
+
   return {
     status: 200,
-    body: 'TODO!'
+    body: `User ${user.id} deleted`
   };
 }
