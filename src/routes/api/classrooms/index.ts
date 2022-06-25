65e0822fd7b8116db454a42fa09a3e5816abeaf3
mystique09 <knightkrypto7@gmail.com>
2022-06-25 13:08:51 +0800

    add: error handling

--- src/routes/api/classrooms/index.ts
+++ src/routes/api/classrooms/index.ts
@@ -1,17 +1,48 @@
 import type { RequestEvent } from "@sveltejs/kit";
+import prisma from "$lib/prisma";
+import { newClass } from "$lib/server/controllers/classroom.controller";
+
+type CreateClass = {
+  admin_id: string;
+  name: string;
+  description: string;
+  section: string;
+  room: string;
+  subject: string;
+}
 
 // endpoint to get all classrooms from a user
-export function get() {
-  return {
-    status: 200,
-    body: 'TODO!'
-  };
+export async function get() {
+  try {
+    const classrooms = await prisma.class.findMany();
+
+    return {
+      status: 200,
+      body: classrooms
+    }
+  } catch (e: any) {
+    return {
+      status: 500,
+      body: e.message
+    }
+  }
 }
 
 // endpoint to create new classroom
-export function post(event: RequestEvent) {
-  return {
-    status: 200,
-    body: 'TODO!'
-  };
+export async function post(event: RequestEvent) {
+  const data: CreateClass = await event.request.json();
+
+  try {
+    const user = await newClass(data);
+
+    return {
+      status: 200,
+      body: user
+    }
+  } catch (e: any) {
+    return {
+      status: 500,
+      body: e.message
+    }
+  }
 }
